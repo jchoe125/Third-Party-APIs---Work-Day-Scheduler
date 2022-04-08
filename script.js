b7780f85e8d9e86071f77627ef00fc39fd9ba02d
Joe Choe <joechoe1989@gmail.com>
2022-04-07 22:07:45 -0700

    final changes to html, css, js files

--- script.js
+++ script.js
@@ -1,71 +1,47 @@
-//TODO use moment to display current date at top of planner
-
+// use moment to display current date at top of planner
 var currentDay = moment();
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-// TODO set variables for id elements using jQuery
-var textarea9 = $("#9AM");
-var textarea10 = $("#10AM");
-var textarea11 = $("#11AM");
-var textarea12 = $("#12PM");
-var textarea13 = $("#1PM");
-var textarea14 = $("#2PM");
-var textarea15 = $("#3PM");
-var textarea16 = $("#4PM");
-var textarea17 = $("#5PM");
+// storing user input and time stamp into local storage upon clicking save button
+$(".saveBtn").on("click", function() {
+    var time = $(this).siblings().attr("id");
+    var text = $(this).siblings(".form-control").val();
+    localStorage.setItem(time, text);
+    console.log(time, text);
+});
 
-// TODO set variables using items retrieved from local storage
-var textareaVal9 = localStorage.getItem("9AM");
-var textareaVal10 = localStorage.getItem("10AM");
-var textareaVal11 = localStorage.getItem("11AM");
-var textareaVal12 = localStorage.getItem("12PM");
-var textareaVal13 = localStorage.getItem("1PM");
-var textareaVal14 = localStorage.getItem("2PM");
-var textareaVal15 = localStorage.getItem("3PM");
-var textareaVal16 = localStorage.getItem("4PM");
-var textareaVal17 = localStorage.getItem("5PM");
-
-// display values from local storage into text area
-textarea9.val(textareaVal9);
-textarea10.val(textareaVal10);
-textarea11.val(textareaVal11);
-textarea12.val(textareaVal12);
-textarea13.val(textareaVal13);
-textarea14.val(textareaVal14);
-textarea15.val(textareaVal15);
-textarea16.val(textareaVal16);
-textarea17.val(textareaVal17);
+// loading saved input from localstorage for each hour block
+$("#9_TA").val(localStorage.getItem("9_clock"));
+$("#10_TA").val(localStorage.getItem("10_clock"));
+$("#11_TA").val(localStorage.getItem("11_clock"));
+$("#12_TA").val(localStorage.getItem("12_clock"));
+$("#13_TA").val(localStorage.getItem("13_clock"));
+$("#14_TA").val(localStorage.getItem("14_clock"));
+$("#15_TA").val(localStorage.getItem("15_clock"));
+$("#16_TA").val(localStorage.getItem("16_clock"));
+$("#17_TA").val(localStorage.getItem("17_clock"));
 
 
-// TODO change color of hour blocks based on current time of day
+// change color of hour blocks based on current time of day
 function timeChecker() {
     var currentTime = moment().hour();
-
-    // using loop function to compare time
-    $(".form-control").each(function() {
-        var timeBlock = parseInt($(this).attr("id"));
+    // using loop function to compare present, past, future time
+    $(".timeBlock").each(function() {
+        var timeBlock = parseInt($(this).children().attr("id"));
+        console.log(timeBlock, currentTime)
         if (timeBlock < currentTime) {
-            $(this).addClass("past")
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
         } else if (timeBlock === currentTime) {
             $(this).removeClass("past");
             $(this).addClass("present");
-        } else {
+            $(this).removeClass("future");
+        } else if (timeBlock > currentTime) {
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
         }
     })
+    timeChecker();
 }
-
-timeChecker();
-
-//TODO add $("button").on("click", function(){} so that entry is saved into local storage
-$(".saveBtn").on("click", function(event) {
-    event.preventDefault();
-    var textarea = $(this).prev();
-    var id = textarea.attr("id");
-    var value = textarea.val();
-    localStorage.setItem(id, value);
-});
-
-//TODO add $("button").on("click, function(){} so that schedule is cleared upon clicking clear schedule button")
